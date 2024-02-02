8d5a669138cea194ac1b23530c083554a15f938f
Borhan <bmoyassari@gmail.com>
2024-02-02 09:03:41 +0330

    setter added to book

--- book.cpp
+++ book.cpp
@@ -1,30 +1,27 @@
 #include "book.h"
 #include <string>
+
 Book::CompTag Book::ms_comp_tag_book = Book::TitleBase;
 
 Book::Book(const std::string &_title, const std::string &_author, const std::string &_publish_date, Genre _genre)
-    : m_title(_title),
-      m_author(_author),
-      m_give_time(0),
-      m_is_available(true),
-      m_publish_date(_publish_date),
-      m_genre(_genre)
-{
+        : m_title(_title),
+          m_author(_author),
+          m_give_time(0),
+          m_is_available(true),
+          m_publish_date(_publish_date),
+          m_genre(_genre) {
     this->m_id = rand();
 }
 
 Book::Book()
-    : m_give_time(0),
-      m_is_available(true)
-{
+        : m_give_time(0),
+          m_is_available(true) {
     this->m_id = rand();
 }
 
-int Book::loadFromFile(const std::string &_file_path)
-{
+int Book::loadFromFile(const std::string &_file_path) {
     std::ifstream target_file(BOOK_DIR + _file_path + ".book");
-    if (!target_file.is_open())
-    {
+    if (!target_file.is_open()) {
         std::cerr << "Book::loadFromFile(string) : Could not open target file\n";
         return -1;
     }
@@ -34,11 +31,9 @@ int Book::loadFromFile(const std::string &_file_path)
     return 0;
 }
 
-int Book::saveToFile()
-{
+int Book::saveToFile() {
     std::ofstream target_file(BOOK_DIR + this->m_title + ".book");
-    if (!target_file.is_open())
-    {
+    if (!target_file.is_open()) {
         std::cerr << "Book::loadFromFile(string) : Could not open target file\n";
         return -1;
     }
@@ -48,57 +43,48 @@ int Book::saveToFile()
     return 0;
 }
 
-void Book::changeCompTag(const CompTag _tag)
-{
+void Book::changeCompTag(const CompTag _tag) {
     ms_comp_tag_book = _tag;
 }
 
-void Book::setAsAvailable()
-{
+void Book::setAsAvailable() {
     this->m_is_available = true;
 }
 
-void Book::setAsUnAvailable()
-{
+void Book::setAsUnAvailable() {
     this->m_is_available = false;
 }
 
-int64_t Book::FirstUserID()
-{
+int64_t Book::FirstUserID() {
     this->m_reservation_queue.front();
 }
 
-int64_t Book::giveTime()
-{
+int64_t Book::giveTime() {
     return this->m_give_time;
 }
 
-bool Book::isReservationEmpty()
-{
+bool Book::isReservationEmpty() {
     return this->m_reservation_queue.isEmpty();
 }
 
-std::string Book::genreEnumToString(const Book::Genre &_genre) const
-{
+std::string Book::genreEnumToString(const Book::Genre &_genre) const {
     std::string s[] = {
-        "Novel",
-        "Fiction",
-        "Historical",
-        "Mystery",
-        "Biography"};
+            "Novel",
+            "Fiction",
+            "Historical",
+            "Mystery",
+            "Biography"};
     return s[_genre];
 }
 
-inputDataStream &operator>>(inputDataStream &inputStream, Book &_book)
-{
+inputDataStream &operator>>(inputDataStream &inputStream, Book &_book) {
     inputStream >> _book.m_title >> _book.m_author >>
-        _book.m_id >> _book.m_genre >> _book.m_give_time >>
-        _book.m_publish_date >> _book.m_is_available >> _book.m_reservation_queue;
+                _book.m_id >> _book.m_genre >> _book.m_give_time >>
+                _book.m_publish_date >> _book.m_is_available >> _book.m_reservation_queue;
     return inputStream;
 }
 
-outputDataStream &operator<<(outputDataStream &outputStream, const Book &_book)
-{
+outputDataStream &operator<<(outputDataStream &outputStream, const Book &_book) {
     outputStream << _book.m_title << _book.m_author
                  << _book.m_id << _book.m_genre << _book.m_give_time
                  << _book.m_publish_date << _book.m_is_available << _book.m_reservation_queue;
@@ -106,20 +92,17 @@ outputDataStream &operator<<(outputDataStream &outputStream, const Book &_book)
     return outputStream;
 }
 
-inputDataStream &operator>>(inputDataStream &inputStream, Book::Genre &_genre)
-{
-    inputStream >> (int32_t &)_genre;
+inputDataStream &operator>>(inputDataStream &inputStream, Book::Genre &_genre) {
+    inputStream >> (int32_t &) _genre;
     return inputStream;
 }
 
-outputDataStream &operator<<(outputDataStream &outputStream, const Book::Genre &_genre)
-{
-    outputStream << (int32_t &)_genre;
+outputDataStream &operator<<(outputDataStream &outputStream, const Book::Genre &_genre) {
+    outputStream << (int32_t &) _genre;
     return outputStream;
 }
 
-std::ostream &operator<<(std::ostream &os, const Book &_book)
-{
+std::ostream &operator<<(std::ostream &os, const Book &_book) {
     os << "Title: " << _book.m_title
        << "\nAuthor: " << _book.m_author
        << "\nGenre: " << _book.genreEnumToString(_book.m_genre)
@@ -127,68 +110,60 @@ std::ostream &operator<<(std::ostream &os, const Book &_book)
     return os;
 }
 
-bool Book::operator<(const Book &_b)
-{
-    if (ms_comp_tag_book == CompTag::TitleBase)
-    {
+bool Book::operator<(const Book &_b) {
+    if (ms_comp_tag_book == CompTag::TitleBase) {
         return this->m_title < _b.m_title;
     }
-    if (ms_comp_tag_book == CompTag::IDBase)
-    {
+    if (ms_comp_tag_book == CompTag::IDBase) {
         return this->m_id < _b.m_id;
     }
 }
 
-bool Book::operator>(const Book &_b)
-{
-    if (ms_comp_tag_book == CompTag::TitleBase)
-    {
+bool Book::operator>(const Book &_b) {
+    if (ms_comp_tag_book == CompTag::TitleBase) {
         return this->m_title > _b.m_title;
     }
-    if (ms_comp_tag_book == CompTag::IDBase)
-    {
+    if (ms_comp_tag_book == CompTag::IDBase) {
         return this->m_id > _b.m_id;
     }
 }
 
-bool Book::operator==(const Book &_b)
-{
-    if (ms_comp_tag_book == CompTag::TitleBase)
-    {
+bool Book::operator==(const Book &_b) {
+    if (ms_comp_tag_book == CompTag::TitleBase) {
         return this->m_title == _b.m_title;
     }
-    if (ms_comp_tag_book == CompTag::IDBase)
-    {
+    if (ms_comp_tag_book == CompTag::IDBase) {
         return this->m_id == _b.m_id;
     }
 }
 
-bool Book::operator!=(const Book &_b)
-{
-    if (ms_comp_tag_book == CompTag::TitleBase)
-    {
+bool Book::operator!=(const Book &_b) {
+    if (ms_comp_tag_book == CompTag::TitleBase) {
         return this->m_title != _b.m_title;
     }
-    if (ms_comp_tag_book == CompTag::IDBase)
-    {
+    if (ms_comp_tag_book == CompTag::IDBase) {
         return this->m_id != _b.m_id;
     }
 }
 
-int64_t Book::id()
-{
+int64_t Book::id() {
     this->m_id;
 }
 
-std::string Book::title()
-{
+std::string Book::title() {
     return this->m_title;
 }
 
-void Book::popLastUserID()
-{
-    if (!this->m_reservation_queue.isEmpty())
-    {
+void Book::popLastUserID() {
+    if (!this->m_reservation_queue.isEmpty()) {
         this->m_reservation_queue.dequeue();
     }
+}
+
+void Book::idSetter(const int64_t &argId) {
+    this->m_id = argId;
+}
+
+void Book::titleSetter(const std::string &argTitle) {
+    this->m_title = argTitle;
 }
--- book.h
+++ book.h
@@ -1,5 +1,6 @@
 #ifndef BOOK_H
 #define BOOK_H
+
 #include <iostream>
 #include <cstdint>
 #include <string>
@@ -10,48 +11,74 @@
 #define BOOK_DIR "./book/"
 
 class OrdinaryUser;
-class Book
-{
+
+class Book {
 
 public:
-    enum Genre
-    {
+    enum Genre {
         Novel,
         Fiction,
         Historical,
         Mystery,
         Biography
     };
-    enum CompTag
-    {
+    enum CompTag {
         IDBase,
         TitleBase,
     };
+
     Book(const std::string &_title, const std::string &_author, const std::string &_publish_date, Genre _genre);
+
     Book();
+
     int loadFromFile(const std::string &_file_path);
+
     int saveToFile();
+
     friend inputDataStream &operator>>(inputDataStream &inputStream, Book &_book);
+
     friend outputDataStream &operator<<(outputDataStream &outputStream, const Book &_book);
+
     friend inputDataStream &operator>>(inputDataStream &inputStream, Genre &_genre);
+
     friend outputDataStream &operator<<(outputDataStream &outputStream, const Genre &_genre);
+
     friend std::ostream &operator<<(std::ostream &os, const Book &_book);
+
     bool operator<(const Book &_b);
+
     bool operator>(const Book &_b);
+
     bool operator==(const Book &_b);
+
     bool operator!=(const Book &_b);
+
     int64_t id();
+
     std::string title();
+
     void popLastUserID();
+
     static void changeCompTag(const CompTag _tag);
+
     void setAsAvailable();
+
     void setAsUnAvailable();
+
     int64_t FirstUserID();
+
     int64_t giveTime();
+
     bool isReservationEmpty();
+
+    void idSetter(const int64_t &argId);
+
+    void titleSetter(const std::string &argTitle);
+
     // friend std::istream& operator>>(std::istream &is,Book& _book);
 private:
     std::string genreEnumToString(const Book::Genre &_genre) const;
+
     std::string m_title;
     std::string m_author;
     std::string m_publish_date;
