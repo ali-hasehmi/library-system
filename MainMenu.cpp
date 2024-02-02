76a0996d954f54c047edd9f6c70f8a29573b1ba1
Borhan <bmoyassari@gmail.com>
2024-02-02 08:01:30 +0330

    wont jump out of program after registering

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,39 +1,35 @@
 #include "MainMenu.h"
 #include <windows.h>
 
-MainMenu::MainMenu()
-{
+MainMenu::MainMenu() {
     Menu mainMenu({"Login", "Signup"});
     int option = mainMenu.display();
-    switch (option)
-    {
-    case 0:
-        std::cout << "Login Selected";
-        if (menu0_loginHandler())
-        {
-            menu1_normalUser();
-        }
-        else
-        {
-            menu1_adminInterface();
-        }
-        break;
-    case 1:
-        std::cout << "Logout Selected";
-        menu0_signUpHandler();
-        break;
+    switch (option) {
+        case 0:
+            std::cout << "Login Selected";
+            if (menu0_loginHandler()) {
+                menu1_normalUser();
+            } else {
+                menu1_adminInterface();
+            }
+            break;
+        case 1:
+            std::cout << "signup Selected";
+            menu0_signUpHandler();
+            if (menu0_loginHandler()) {
+                menu1_normalUser();
+            } else {
+                menu1_adminInterface();
+            }
+            break;
     }
 }
 
-void MainMenu::menu0_signUpHandler()
-{
+void MainMenu::menu0_signUpHandler() {
     int res = 0;
-    do
-    {
-
+    do {
         system("cls");
-        if (res)
-        {
+        if (res) {
             std::cout << "User Already Exists!\n";
             Sleep(1000);
         }
@@ -59,141 +55,129 @@ void MainMenu::menu0_signUpHandler()
     std::cout << "user created Successfully;";
     Sleep(1000);
     system("cls");
+
 }
 
-bool MainMenu::menu0_loginHandler()
-{
-    system("cls");
-    std::cout << "Login Selected\n";
-    std::string userName;
-    std::string userPassword;
-    std::cout << "Enter Username : ";
-    std::cin >> userName;
-    std::cout << "Enter Password : ";
-    std::cin >> userPassword;
-    if (userName == "admin")
-    {
-        /*   user is admin the compare password and etc
-                if (passwordCompare == true) */
-        return 0;
-    }
-    else
-    {
-        /*        compare passwords for user
-                if (passwordCompare == true) */
-        OrdinaryUser ou;
-        if(ou.logIn(userName,userPassword) == nullptr){
-            std::cout << "user not found\n";
+bool MainMenu::menu0_loginHandler() {
+    while (true) {
+        system("cls");
+        std::cout << "Login Selected\n";
+        std::string userName;
+        std::string userPassword;
+        std::cout << "Enter Username : ";
+        std::cin >> userName;
+        std::cout << "Enter Password : ";
+        std::cin >> userPassword;
+        if (userName == "admin") {
+            /*   user is admin the compare password and etc
+                    if (passwordCompare == true) */
+            return false;
+        } else {
+            /*        compare passwords for user
+                    if (passwordCompare == true) */
+            OrdinaryUser ou;
+            if (ou.logIn(userName, userPassword) == nullptr) {
+                std::cout << "user not found\n";
+            } else {
+                std::cout << "logged in successfully\n";
+                return true;
+            }
         }
-        else{
-            std::cout << "logged in successfully\n";
-        }
-        return 1;
     }
 }
 
-void MainMenu::menu1_normalUser()
-{
+void MainMenu::menu1_normalUser() {
     Menu normalUserMenu1({"Show All Books", "Show My Books", "Search Books", "Sort Books", "Exit"});
     bool loopCondition = true;
-    while (loopCondition)
-    {
+    while (loopCondition) {
         int option = normalUserMenu1.display();
-        switch (option)
-        {
-        case 0:
-            // show all books
-            break;
-        case 1:
-            // show my books
-            break;
-        case 2:
-            // search books
-            break;
-        case 3:
-            // sort books
-            break;
-        case 4:
-            loopCondition = false;
-            break;
-        default:
-            std::cout << "\nInvalid Option";
-            break;
+        switch (option) {
+            case 0:
+                // show all books
+                break;
+            case 1:
+                // show my books
+                break;
+            case 2:
+                // search books
+                break;
+            case 3:
+                // sort books
+                break;
+            case 4:
+                loopCondition = false;
+                break;
+            default:
+                std::cout << "\nInvalid Option";
+                break;
         }
     }
 }
 
-void MainMenu::menu1_adminInterface()
-{
+void MainMenu::menu1_adminInterface() {
     Menu adminMenu1({"Get Book", "Give Book", "Reserve", "ReNew", "Exit"});
     bool loopCondition = true;
-    while (loopCondition)
-    {
+    while (loopCondition) {
         int option = adminMenu1.display();
-        switch (option)
-        {
+        switch (option) {
             //                Get Book
-        case 0:
-        {
-            system("cls");
-            std::string bookTitle;
-            std::string userName;
-            std::cout << "Enter book name : ";
-            std::cin >> bookTitle;
-            std::cout << "Enter book owner : ";
-            std::cin >> userName;
-            system("cls");
-            // code related to changing book ownership goes here
-            break;
-        }
-        case 1:
-        {
-            system("cls");
-            std::string bookTitle;
-            std::string userName;
-            std::cout << "Enter book name : ";
-            std::cin >> bookTitle;
-            std::cout << "Enter book owner : ";
-            std::cin >> userName;
-            system("cls");
-            // code related to giving book goes here
-            break;
-        }
-        case 2:
-        {
-            system("cls");
-            std::string bookTitle;
-            std::string userName;
-            std::cout << "Enter book name : ";
-            std::cin >> bookTitle;
-            std::cout << "Enter book owner : ";
-            std::cin >> userName;
-            system("cls");
-            //                code related to reserving book
-            break;
-        }
-        case 3:
-        {
-            system("cls");
-            std::string bookTitle;
-            std::string userName;
-            int extendedPeriod;
-            std::cout << "Enter book name : ";
-            std::cin >> bookTitle;
-            std::cout << "Enter book owner : ";
-            std::cin >> userName;
-            std::cout << "Enter time to extend : ";
-            std::cin >> extendedPeriod;
-            system("cls");
-            //                code to reNew
-            break;
-        }
-        case 4:
-            loopCondition = false;
-            break;
-        default:
-            std::cout << "\nInvalid Option";
-            break;
+            case 0: {
+                system("cls");
+                std::string bookTitle;
+                std::string userName;
+                std::cout << "Enter book name : ";
+                std::cin >> bookTitle;
+                std::cout << "Enter book owner : ";
+                std::cin >> userName;
+                system("cls");
+                // code related to changing book ownership goes here
+                break;
+            }
+            case 1: {
+                system("cls");
+                std::string bookTitle;
+                std::string userName;
+                std::cout << "Enter book name : ";
+                std::cin >> bookTitle;
+                std::cout << "Enter book owner : ";
+                std::cin >> userName;
+                system("cls");
+                // code related to giving book goes here
+                break;
+            }
+            case 2: {
+                system("cls");
+                std::string bookTitle;
+                std::string userName;
+                std::cout << "Enter book name : ";
+                std::cin >> bookTitle;
+                std::cout << "Enter book owner : ";
+                std::cin >> userName;
+                system("cls");
+                //                code related to reserving book
+                break;
+            }
+            case 3: {
+                system("cls");
+                std::string bookTitle;
+                std::string userName;
+                int extendedPeriod;
+                std::cout << "Enter book name : ";
+                std::cin >> bookTitle;
+                std::cout << "Enter book owner : ";
+                std::cin >> userName;
+                std::cout << "Enter time to extend : ";
+                std::cin >> extendedPeriod;
+                system("cls");
+                //                code to reNew
+                break;
+            }
+            case 4:
+                loopCondition = false;
+                break;
+            default:
+                std::cout << "\nInvalid Option";
+                break;
         }
     }
 }
